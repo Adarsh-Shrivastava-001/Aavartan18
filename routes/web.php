a94d0e937c548dca8dd5bacfb3e32b784ebc57a3
Akash Gayakwad <akashgayakwad123@gmail.com>
2018-08-29 03:25:26 +0530

    routes created

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,35 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('aavartan');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/vigyaan', function () {
+    return view('vigyaan');
+});
+Route::get('/events', function () {
+    return view('events');
+});
+Route::get('/gallery', function () {
+    return view('gallery');
+});
+Route::get('/attractions', function () {
+    return view('attractions');
+});
+Route::get('/initiatives', function () {
+    return view('initiatives');
+});
+Route::get('/login', function () {
+    return view('loginsignup');
+});
+Route::get('/sponsors', function () {
+    return view('sponsors');
+});
+Route::get('/schedule', function () {
+    return view('schedule');
+});
+Route::get('/contactus', function () {
+    return view('contactus');
 });
