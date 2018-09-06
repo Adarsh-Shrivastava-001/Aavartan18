249b709450ad01ff499b3d70c2d98a72a5dcb0fd
siddharthshah3030 <siddharthshah3030@gmail.com>
2018-09-07 02:15:40 +0530

    gallery display fixed and css modified

--- S_gallery/js/change.js
+++ S_gallery/js/change.js
@@ -2,19 +2,22 @@
 onClick = "getIndex(this);"
 
 function getIndex(node) {
-  console.log(node.value)
-  var childs = node.childNodes;
-  for (i = 0; i < childs.length; i++) {
-    if (node == childs[i]) break;
-    console.log(`loop ${i}`)
-  }
-  console.log(childs)
+  // console.log(node.value)
+  // var childs = node.childNodes;
+  // for (i = 0; i < childs.length; i++) {
+  //   if (node == childs[i]) break;
+  //   console.log(`loop ${i}`)
+  // }
+  // console.log(childs)
 
 globalpiece = node.value -1;
 document.getElementById("changerButton").click();
 
 }
-
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 // var lis = document.getElementById("rig").getElementsByTagName('li');
 //
 //   for (var i=0; i<lis.length; i++) {
